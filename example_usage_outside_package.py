f7488744cbc2a91359df2470700fd0ec554ed28f
ASPP Student <student@localhost>
2023-06-28 12:04:50 -0600

    We eddit Python expert potion

--- example_usage_outside_package.py
+++ example_usage_outside_package.py
@@ -1,1 +1,6 @@
-# Task: import make_example_potion from the module brew_potions.py
+# Task: import make_example_potion from the module brew_potions.
+#import brewing
+import brewing.brew_potions as br
+
+#brewing.brew_potions.make_example_potion('Hi')
+br.make_example_potion('Hi')
