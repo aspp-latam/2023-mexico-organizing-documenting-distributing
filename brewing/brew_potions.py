9fb9d8f02cfca6bfed55d6e7936c414abdf070c4
ASPP Student <student@localhost>
2023-06-28 10:58:52 -0600

    fixes brewing packge naming problem

--- brewing/brew_potions.py
+++ brewing/brew_potions.py
@@ -1,15 +1,15 @@
-import potion_class
-import containers
-import cooking
-import inspection
+import brewing.potion_class
+import brewing.containers
+import brewing.cooking
+import brewing.inspection
 
 
 def make_example_potion(student_name):
-    my_potion = potion_class.Potion(student_name=student_name)
+    my_potion = brewing.potion_class.Potion(student_name=student_name)
     # Set up your old kettle and light an eternal flame underneath it.
-    my_potion.setup(container=containers.old_kettle, heat_source=cooking.eternal_flame)
+    my_potion.setup(container=brewing.containers.old_kettle, heat_source=brewing.cooking.eternal_flame)
     # Simmer for 5 hours.
-    cooking.simmer(my_potion, duration=5)
+    brewing.cooking.simmer(my_potion, duration=5)
     print("You have successfully run make_example_potion, well done :).")
     return my_potion
 
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     my_name = 'ASPP student'
     my_potion = make_example_potion(student_name=my_name)
     # Let Snape inspect the potion
-    inspection.inspection_by_Snape(potion=my_potion, target_potion='example_potion')
+    brewing.inspection.brewing.inspection_by_Snape(potion=my_potion, target_potion='example_potion')
