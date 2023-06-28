968e4242e019213d430f6b67762255a25ae83ec1
ASPP Student <student@localhost>
2023-06-28 11:47:44 -0600

    Our solution for the exercise 3

--- brewing/example_usage_within_package.py
+++ brewing/example_usage_within_package.py
@@ -1,1 +1,7 @@
 # Task: import make_example_potion from the module brew_potions.py
+
+#from brew_potions import make_example_potion
+import brew_potions as br 
+ 
+br.make_python_expert_potion('andres')
+
